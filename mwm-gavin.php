eb877c14f41a6ea0ebe6a1743f8c30e9261a3eb5
Inma0711 <inmagonzalez045@gmail.com>
2025-07-07 13:51:58 +0200

    Añadido cambio de texto de link generator

--- mwm-gavin.php
+++ mwm-gavin.php
@@ -11,22 +11,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-/**************************************************
- * Personalización: Ocultar pestaña "Visits" en el panel de YITH Affiliates
- * Descripción: Elimina la pestaña de visitas del menú de afiliados en el admin.
- * Proyecto: Belush / Gavin
- **************************************************/
-add_filter('yith_wcaf_affiliates_settings', function($options) {
-    if (isset($options['affiliates']['affiliates_options']['sub-tabs']['affiliates-clicks'])) {
-        unset($options['affiliates']['affiliates_options']['sub-tabs']['affiliates-clicks']);
-    }
-    return $options;
-}, 20);
 
-/**************************************************
- * Cargar JS personalizado solo en el admin
- **************************************************/
-add_action('admin_enqueue_scripts', function() {
+add_action('wp_enqueue_scripts', function() {
     wp_enqueue_script(
         'mwm-gavin-admin-custom',
         plugin_dir_url(__FILE__) . 'admin-custom.js',
